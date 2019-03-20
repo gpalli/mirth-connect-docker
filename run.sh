5acabcbb804c7b28c958bf3a865e97ca0f5045ab
Guillermo Palli <guillermo.palli@gmail.com>
2019-03-20 17:51:53 -0300

    Agrego literales

--- run.sh
+++ run.sh
@@ -3,10 +3,14 @@
 # if secret file with runtime parameters exists
 if [ -r /etc/secret-volume/mirth.properties ]; then
     cp -f /etc/secret-volume/mirth.properties /opt/mirth-connect/conf/mirth.properties
+    echo "Configuring mirth.properties"
 fi
 # if secret file with runtime parameters exists
 if [ -r /etc/secret-volume/keystore.jks ]; then
+  mkdir -p /opt/mirth-connect/appdata
   cp -f /etc/secret-volume/keystore.jks /opt/mirth-connect/appdata/keystore.jks
+  echo "Configuring keystore.jks"
 fi
 
+echo "Launching Mirth Connect Server..."
 java -Duser.timezone="America/Argentina/Buenos_Aires" -jar mirth-server-launcher.jar
